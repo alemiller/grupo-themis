e75c93dd0a3b91da776c079390e2d3c7b5624c47
Alejandro Miller <alejandro.miller@univtec.com>
2017-06-13 13:28:15 -0300

    send email changes

--- application/models/send_grid_email_model.php
+++ application/models/send_grid_email_model.php
@@ -10,17 +10,30 @@ class Send_grid_email_model extends CI_Model {
 
     public function send_email($title, $data, $view) {
 
-        $sendgrid = new SendGrid('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');
-        $email = new SendGrid\Email();
+//        $sendgrid = new SendGrid('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');
+//        $email = new SendGrid\Email();
+//
+//        $message = $this->load->view('templates/emails/' . $view, $data, TRUE);
+//
+//        $email->addTo($data->cliente->email)
+//                ->setFrom('info@grupo-themis.com.ar')
+//                ->setSubject($title)
+//                ->setHtml($message);
+//
+//        $sendgrid->send($email);
 
         $message = $this->load->view('templates/emails/' . $view, $data, TRUE);
+        $from = new SendGrid\Email(null, 'info@grupo-themis.com.ar');
+        ;
+        $to = new SendGrid\Email(null, $data->cliente->email);
+        $content = new SendGrid\Content("text/html", $message);
+        $mail = new SendGrid\Mail($from, $title, $to, $content);
 
-        $email->addTo($data->cliente->email)
-                ->setFrom('info@grupo-themis.com.ar')
-                ->setSubject($title)
-                ->setHtml($message);
+        $apiKey = getenv('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');
+        $sg = new \SendGrid($apiKey);
 
-        $sendgrid->send($email);
+        $response = $sg->client->mail()->send()->post($mail);
+        echo $response->statusCode();
     }
 
 }
