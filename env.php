da0bfe66b5b6bfd14621b5afb29d98c4321d23fb
Alejandro Miller <alejandro.miller@univtec.com>
2017-06-13 10:14:08 -0300

    environment

--- env.php
+++ env.php
@@ -1,5 +1,5 @@
 <?php
 
-define('ENVIRONMENT', 'development');
+define('ENVIRONMENT', 'production');
 define('CLEARDB_DATABASE_URL', 'mysql://b4984992474989:0327ae0d@us-cdbr-iron-east-03.cleardb.net/heroku_a658099100e6383?reconnect=true');
 
